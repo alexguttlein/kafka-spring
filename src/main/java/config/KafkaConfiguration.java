d29ed1981d015af9169002215d0b4c290824b093
alexguttlein <alexisguttlein@gmail.com>
2025-08-09 10:20:23 -0300

    se agrega configuracion de kafka

--- src/main/java/config/KafkaConfiguration.java
+++ src/main/java/config/KafkaConfiguration.java
@@ -1,2 +1,53 @@
-package config;public class KafkaConfiguration {
+package config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    /**
+     * Mapa que contiene las configuraciones del Consumer
+     * @return Map<String, Object> properties
+     */
+    public Map<String, Object> consumerProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "alex-group");
+//        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+//        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+//        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return properties;
+    }
+
+    /**
+     * Se crea ConsumerFactory
+     * @return ConsumerFactory<String, String>
+     */
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerProperties());
+    }
+
+    /**
+     * Se crea ConcurrentKafkaListenerContainerFactory
+     * @return ConcurrentKafkaListenerContainerFactory<String, String>
+     */
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory () {
+        ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        listenerContainerFactory.setConsumerFactory(consumerFactory());
+        return listenerContainerFactory;
+    }
 }
